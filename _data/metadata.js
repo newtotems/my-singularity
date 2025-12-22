7420b444b4b7582cd714e4677a571a9b25560c1e
david-hogg <71979980+newtotems@users.noreply.github.com>
2025-12-22 18:16:11 +0000

    blog
    .

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,5 @@
 export default {
-	title: "My Singularity",
-	url: "https://mysingularity.blog/",
+	title: "Helianity",
+	url: "https://helianity.com",
 	language: "en",
-	description: "In 2029, the world is predicted to hit a technological tipping point - the dawn of human-level AI. In that same year, I will hit my own tipping point: turning 50. My Singularity is a blog about how I am preparing for both. It is a chronicle of future-proofing my mind, body, and purpose so that my midlife isn't a crisis, but an upgrade."
-}
+	description: ""}
--- content/blog/firstpost.md
+++ content/blog/firstpost.md
@@ -1,7 +1,9 @@
 ---
-title: A joint Singularity
-description: A joint Singularity
+title: I am a Helian
+description: 
 date: 2026-01-01
 tags: 
 ---
-This
+I consider myself the first Helian, but I hope there will be many. In fact I hope one day that all Humans will also be Helians.
+
+To be a Helian is to look 
